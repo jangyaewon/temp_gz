9809b762231c926a8d9414df28f78969ba01aa21
Jang Yaewon <rainsimple@naver.com>
2018-07-24 09:30:13 +0000

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,33 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'seoul_dong.csv'))
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'woman.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'UTF-8')
 csv.each do |row|
-   r = Road.new
-   r1 = Road.where(state_id: 17, road_name: row['읍면동명'])[0]
-   p row['읍면동명']
-   
-   if r1.nil?
-    r.road_name = row['읍면동명']
-    r.state_id = 17
-    r.district_id = 250
+ 
+  r = Restaurant.new
+  r =Restaurant.where(res_name: row['사용장소'], detail_addr: row['상세주소'])[0]
+  
+  if r.nil?
+    r1 = Restaurant.new
+    ro = Road.new
+    ro = Road.find_by_road_name(row['road2'])
+    p ro.id
+    
+    r1.g_name = row['사용기관']
+    r1.res_name = row['사용장소']
+    r1.detail_addr = row['상세주소']
+    r1.food_type = row['종류']
+    r1.state_id = 1
+    r1.road_id = ro.id
+    r1.district_id = ro.district_id
+    r1.r_count = 1
+    r1.latitude = row['Latitude']
+    r1.longitude = row['Longitude']
+    contents = row['contents']
+    r1.save
+    p row['road2']
+  else
+    r.r_count += 1 
     r.save
-   end
-end
+  end
+  end
