5ee391d447a8f7d2e957fba82881902607941a06
Jang Yaewon <rainsimple@naver.com>
2018-07-24 08:48:33 +0000

    seed_d

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,14 @@ require 'csv'
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'seoul_dong.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'UTF-8')
 csv.each do |row|
-   s = State.new
+   d = District.new
    s1 = State.where(state_name: row['시도명'])[0]
-   p row['시도명']
+   d1 = District.where(state_id: s1.id, district_name: row['시군구명'])[0]
+   p row['시군구명']
    
-   if s1.nil?
-    s.state_name = row['시도명']
-    s.save
+   if d1.nil?
+    d.district_name = row['시군구명']
+    d.state_id = s1.id
+    d.save
    end
 end
