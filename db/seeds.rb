5e1eef60cdeb13da93879c7b47ac317c75133947
Jang Yaewon <rainsimple@naver.com>
2018-07-24 09:04:17 +0000

    seed_sj

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,13 @@ csv_text = File.read(Rails.root.join('lib', 'seeds', 'seoul_dong.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'UTF-8')
 csv.each do |row|
    r = Road.new
-   s1 = State.where(state_name: row['시도명'])[0]
-   d1 = District.where(state_id: s1.id, district_name: row['시군구명'])[0]
-   r1 = Road.where(state_id: s1.id, district_id: d1.id, road_name: row['읍면동명'])[0]
+   r1 = Road.where(state_id: 17, road_name: row['읍면동명'])[0]
    p row['읍면동명']
    
    if r1.nil?
     r.road_name = row['읍면동명']
-    r.state_id = s1.id
-    r.district_id = d1.id
+    r.state_id = 17
+    r.district_id = 250
     r.save
    end
 end
