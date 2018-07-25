31109bb511595dd0345279294a0e10711a4bb6ec
Jang Yaewon <rainsimple@naver.com>
2018-07-25 01:42:18 +0000

    tete

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -12,6 +12,7 @@ class Restaurant < ApplicationRecord
     has_many         :users, through: :scrabs
     has_many         :reviews
     has_and_belongs_to_many :tags
+    has_many         :postings
     
     after_commit :scan_hashtag_from_body, on: :create
     after_commit :update_hashtag_from_body, on: :update
@@ -25,11 +26,11 @@ class Restaurant < ApplicationRecord
  
     def self.search_restaurant_ad(condition)
         unless condition.strip.empty?
-            Restaurant.where("res_name LIKE ?","%#{condition}%")
+            Restaurant.where("detail_addr LIKE ?","%#{condition}%")
         end
     end
 
-    #hashtag
+
     def update_hashtag_from_body
     	restaurant = Restaurant.find_by(id: self.id)
         hashtags = self.contents.split('#')
@@ -37,21 +38,12 @@ class Restaurant < ApplicationRecord
             hashtags[1..-1].map do |hashtag|
                     next if self.tags.where(name: hashtag).first
                     tag= Tag.find_or_create_by(name: hashtag.downcase.strip)
-                    
-                    RestaurantsTag.create(restaurant_id: self.id, tag_id: tag.id)
+                                  RestaurantsTag.create(restaurant_id: self.id, tag_id: tag.id)
             end
         end
-        #uniq : if user two same hashtags,then only one hashtag register
-        # hashtags.uniq.map do |hashtag|
-        #     #find : find specific tag
-        #     #
-        #     tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
-        #     #<< : push the end of the array
-        #     restaurant.tags << tag
-        # end
     end
-    
-    def scan_hashtag_from_body
+ 
+ def scan_hashtag_from_body
         restaurant = Restaurant.find_by(id: self.id)
         hashtags = self.contents.split('#')
         transaction do
@@ -61,6 +53,34 @@ class Restaurant < ApplicationRecord
                     p tag
                  RestaurantsTag.create(restaurant_id: self.id, tag_id: tag.id)
             end
+        end
+ end
+ 
+ 
+    def blog_desc(temp, id)
+        agent = Mechanize.new
+        r = Restaurant.find(id)
+        [1,2,3,4,5].each do |index|
+            
+            page = agent.get"https://search.naver.com/search.naver?where=post&sm=tab_jum&query=#{temp}+#{r.res_name}"
+            list = page.search("li#sp_blog_#{index}.sh_blog_top")
+            date =list.at("dd.txt_inline").inner_text
+            desc = list.at("dd.sh_blog_passage").inner_text
+            b_user = list.at("a.txt84").inner_text
+            
+            url = list.at("a.sh_blog_title").attributes["href"].text
+            img = list.at("img.sh_blog_thumbnail").attributes["src"].text
+            title = list.at("a.sh_blog_title").attributes["title"].text
+            
+            @post = Posting.new
+            @post.date = date
+            @post.desc = desc
+            @post.b_user = b_user
+            @post.url = url
+            @post.b_img = img
+            @post.title = title
+            @post.restaurant_id =id
+            @post.save
         end
     end
 end
